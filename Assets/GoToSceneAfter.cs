6042eff55273213a9c168e345b822db1d48544f2
Jbat1Jumper <3hb.tch@gmail.com>
2015-01-24 17:40:20 -0300

    assets a medio subir, intro, y un triangulo

---
+++ Assets/GoToSceneAfter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToSceneAfter : MonoBehaviour {
+	
+	public float Seconds = 4F;
+	public string Destination = "";
+
+	private float ElapsedSeconds = 0F;
+	// Use this for initialization
+	void Start () {
+		ElapsedSeconds = 0F;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ElapsedSeconds += Time.deltaTime;
+		 
+		if(ElapsedSeconds >= Seconds)
+			Application.LoadLevel (Destination);
+	}
+}

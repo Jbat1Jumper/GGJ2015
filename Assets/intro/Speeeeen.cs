02af916a7db392bc929abfb383c8da7bc2936438
Jbat1Jumper <3hb.tch@gmail.com>
2015-01-25 05:03:44 -0300

    intro

---
+++ Assets/intro/Speeeeen.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Speeeeen : MonoBehaviour {
+
+	public float Speed = 10.0F;
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate (new Vector3 (0, 0, 1), Speed * Time.deltaTime);
+	}
+}

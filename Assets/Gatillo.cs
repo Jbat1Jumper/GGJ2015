4130f32810fe4c514be8efcabc12ec8af8c20db3
Jbat1Jumper <3hb.tch@gmail.com>
2015-01-25 13:26:01 -0300

    Script de gatillo y update para las balas

---
+++ Assets/Gatillo.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gatillo : MonoBehaviour {
+
+	public Bala[] Objetivos = null;
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("OnTriggerEnter");
+		if (other.gameObject.GetComponent<Enano1> () == null) {
+			Debug.Log("No es un enano, solo los enanos pueden gatillarme");
+			return; // No es un enano
+		}
+		PlaySound ();
+
+		if (Objetivos != null) {
+			foreach(var Objetivo in Objetivos)
+						Objetivo.Fire ();
+		}
+	}
+
+	void PlaySound() {
+		Debug.Log ("Ups!");
+	}
+}
--- Assets/test/Bala.cs
+++ Assets/test/Bala.cs
@@ -5,13 +5,20 @@ public class Bala : MonoBehaviour {
 
 	public float Velocidad = -10;
 	public Vector3 Direccion = new Vector3(1, 0, 0);
+	public bool EsperarUnTrigger = false;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+	public void Fire() {
+		EsperarUnTrigger = false;
+	}
+
 	// Update is called once per frame
 	void Update () {
-		this.transform.position += Direccion * (Velocidad * Time.deltaTime);
+		if(!EsperarUnTrigger)
+			this.transform.position += Direccion * (Velocidad * Time.deltaTime);
 	}
 }

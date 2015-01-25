0ab9022c00259c7dd675be378ee146857181a098
Jbat1Jumper <3hb.tch@gmail.com>
2015-01-25 09:59:53 -0300

    music box a medio hacer

---
+++ Assets/MusicBox.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicBox : MonoBehaviour {
+
+	void Awake () {
+		GameObject mb = GameObject.Find("MusicBox");
+		if (mb != null)
+			Destroy (this.gameObject);
+		
+		DontDestroyOnLoad(gameObject);
+	}
+
+	public static MusicBox GetInstance() {
+		return Object.FindObjectOfType<MusicBox> ();
+	}
+}
